626ab006176f7d2e2190d3b7ea7c41df09885438
Bobo Shone <boboboa32@gmail.com>
2013-10-25 16:06:04 +0800

    update.

--- SKHelper.h
+++ SKHelper.h
@@ -13,6 +13,8 @@
 #define skColor4(r, g, b, a) [SKColor colorWithRed:r/255.0f green:g/255.0f blue:b/255.0f alpha:a]
 #define skColor3(r, g, b) skColor4(r, g, b, 1.0f)
 #define skRandom(x,y) (x + arc4random() % (int)(y-x))
+#define skAngle2radian(angle) (angle * M_PI / 180)
+#define skRadian2Angle(radian) (radian * 180 / M_PI)
 
 static inline CGPoint skpNormalize(CGPoint pt) {
     if (pt.x == 0 && pt.y == 0)
